ef9021bf47581e878d2e642860481153a39aff70
nggurbanov <153464213+nggurbanov@users.noreply.github.com>
2023-12-13 13:39:41 +0300

    Update add.py

--- add.py
+++ add.py
@@ -1,1 +1,7 @@
-voila gubanov_project.ipynb
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import statistics
